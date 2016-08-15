0260dc0d4dfac62f8a1c8b2c21376ed135eb39b9
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2016-08-15 22:58:45 +0900

    Initial commit
    This includes binaries.

---
+++ Bug41131/Bug41131/App.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace Bug41131
+{
+    public class App : Application
+    {
+        public App ()
+        {
+            var listener = new System.Net.HttpListener ();
+            listener.Prefixes.Add ("http://*:8080/");
+            listener.Start ();
+            var bytes = System.Text.Encoding.ASCII.GetBytes ("A Proof of Concept for Bug 41131");
+            var context = listener.GetContext ();
+            context.Response.OutputStream.Write (bytes, 0, bytes.Length);
+            context.Response.Close ();
+            listener.Close ();
+        }
+    }
+}
